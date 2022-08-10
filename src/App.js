14945ef38702de19bbfdb15a14b93a83f44fc0ad
Aakash Kumar Singh <aakashsingh3136@gmail.com>
2022-08-11 00:12:07 +0530

    linktree profile added

--- src/App.js
+++ src/App.js
@@ -4,10 +4,6 @@ import Navbar from "./MyComponents/Navbar.js";
 import Slider from "./MyComponents/Slider.js";
 import Heading from "./MyComponents/Heading.js";
 import NoticeSpace from "./MyComponents/Noticespace.js";
-import Resultspace from "./MyComponents/Resultspace.js";
-import Studyspace from "./MyComponents/Studyspace.js";
-import Examspace from "./MyComponents/Examspace.js";
-import Internspace from "./MyComponents/Internspace.js";
 import HotAccessories from "./MyComponents/HotAccessories.js";
 import Footer from "./MyComponents/Footer.js";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
@@ -37,13 +33,7 @@ function App() {
     }
   }, [result]);
 
-  // {
-  //   categories:[
-  //     "categoryName":[
-  //       {data1},{data2}
-  //     ]
-  //   ]
-  // }
+
 
   return (
     <Router>
@@ -65,85 +55,6 @@ function App() {
           </div>
         );
       })}
-
-      {/* <Heading text={"NOTICE SPACE"} />
-      <Routes>
-        <Route
-          path="/university"
-          element={<HotAccessories music={data.hotAccessories.music} />}
-        />
-        <Route path="/hostel" />
-      </Routes>
-
-      <Heading text={"RESULT SPACE"} />
-      <Resultspace />
-      <Routes>
-        <Route
-          path="/2019result"
-          element={<HotAccessories music={data.hotAccessories.music} />}
-        />
-        <Route path="/2020result" />
-        <Route path="/2021result" />
-        <Route
-          path="/2022result"
-          element={<HotAccessories music={data.hotAccessories.music} />}
-        />
-      </Routes>
-
-      <Heading text="STUDY SPACE" />
-      <Studyspace />
-      <Routes>
-        <Route
-          path="/coe"
-          element={<HotAccessories music={data.hotAccessories.music} />}
-        />
-        <Route path="it" />
-        <Route path="/se" />
-        <Route path="/mce" />
-        <Route path="/ece" />
-        <Route path="/ee" />
-        <Route path="/me" />
-        <Route path="/pe" />
-        <Route path="/mae" />
-        <Route path="/civil" />
-        <Route path="/chemical" />
-        <Route path="/pct" />
-        <Route path="/ep" />
-        <Route path="/ene" />
-        <Route path="/bt" />
-      </Routes>
-
-      <Heading text="EXAM SPACE" />
-      <Examspace />
-      <Routes>
-        <Route
-          path="/coeexam"
-          element={<HotAccessories music={data.hotAccessories.music} />}
-        />
-        <Route path="itexam" />
-        <Route path="/seexam" />
-        <Route path="/mceexam" />
-        <Route path="/eceexam" />
-        <Route path="/eeexam" />
-        <Route path="/meexam" />
-        <Route path="/peexam" />
-        <Route path="/maeexam" />
-        <Route path="/civilexam" />
-        <Route path="/chemicalexam" />
-        <Route path="/pctexam" />
-        <Route path="/epexam" />
-        <Route path="/eneexam" />
-        <Route path="/btexam" />
-      </Routes>
-      <Heading text="INTERN / PLACEMENT SPACE" />
-      <Internspace />
-      <Routes>
-        <Route
-          path="/intern"
-          element={<HotAccessories music={data.hotAccessories.music} />}
-        />
-        <Route path="/placement" />
-      </Routes> */}
       <Footer />
     </Router>
   );
--- src/MyComponents/Navbar.js
+++ src/MyComponents/Navbar.js
@@ -21,7 +21,7 @@ const Navbar = () => {
       <a className="navlinks" href="http://exam.dtu.ac.in/result.htm" target="_blank" rel="noopener noreferrer">Results</a>
       <a className="navlinks" href="https://cumsdtu.in/registration_student/login/login.jsp?courseRegistration" target="_blank" rel="noopener noreferrer">Course Registration</a>
       <a className="navlinks" href="https://cumsdtu.in/student_dtu/login/login.jsp" target="_blank" rel="noopener noreferrer">ERP Login</a>
-      <a className="navlinks" href="http://library.dtu.ac.in/" target="_blank" rel="noopener noreferrer">Library Website</a>
+      <a className="navlinks" href="https://rm.dtu.ac.in/login" target="_blank" rel="noopener noreferrer">DTU RM</a>
       <a className="navlinks" href="http://tnp.dtu.ac.in/" target="_blank" rel="noopener noreferrer">T & P Department</a>
       <Link className="navlinks" to="/team">Meet Our Team</Link>
 
